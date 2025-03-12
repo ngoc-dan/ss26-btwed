67330dbe303d6aafd4ce24b7531cb0fcb2726392
ngoc-dan <ngocdan191026@gmail.com>
2025-03-12 16:16:11 +0700

    Add files via upload

--- ss26-bai6.js
+++ ss26-bai6.js
@@ -1,28 +1,25 @@
-let n = +prompt("Nhập độ dài của mảng"); 
+let n = +prompt("Nhập độ dài của mảng");
 let array = [];
 function convertDateFormat(arr, n) {
     if (n === 0) {
         return "Mảng không có phần tử nào";
     } else {
-        let result = [];
+        let result = "";
         for (let i = 0; i < n; i++) {
-            let [year, month, day] = arr[i].split("-"); 
-            result.push(`${day}/${month}/${year}`); 
+            if (/^\d{4}-\d{2}-\d{2}$/.test(arr[i])) {
+                let [year, month, day] = arr[i].split("-");
+                result= result +" "+(`${day}/${month}/${year}`);
+            }
         }
-        return result; 
+        return result;
     }
 }
 if (n >= 0) {
     for (let i = 0; i < n; i++) {
         let newElement = prompt(`Nhập phần tử thứ ${i + 1} (YYYY-MM-DD):`);
-        if (/^\d{4}-\d{2}-\d{2}$/.test(newElement)) {
             array.push(newElement);
-        } else {
-            alert("Định dạng ngày không hợp lệ! Vui lòng nhập lại.");
-            i--; 
-        }
     }
-    console.log("Mảng sau khi chuyển đổi:", convertDateFormat(array, n));
+    alert(convertDateFormat(array, n));
 } else {
     alert("Dữ liệu không hợp lệ");
 }
